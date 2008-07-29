917e3dbef79eeadd3d0728d1cc8f7c46cdbb65db
pmatiello <pmatiello@70df0079-b534-0410-988b-a5721c0f2d16>
2008-07-29 12:09:00 +0000

    docstrings

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	./setup.py install
 
 docs: graph/*.py
-	epydoc -v --no-frames --no-sourcecode --name="python-graph" --url="http://code.google.com/p/python-graph/" --html -o docs graph/*.py
+	epydoc -v --no-frames --no-sourcecode --name="python-graph" --url="http://code.google.com/p/python-graph/" --no-private --html -o docs graph/*.py
 
 edit: graph/*.py
 	gedit graph/*.py &
--- graph/__init__.py
+++ graph/__init__.py
@@ -46,9 +46,6 @@ class graph:
 	"""
 	Graph class.
 
-	Basic operations are defined in this file.
-	Algorithms should refer to external files.
-	
 	@sort: __init__, __len__, __str__, read, write, add_arrow, add_edge, add_graph, add_nodes, add_spanning_tree, del_arrow, del_edge, get_arrow_weight, get_edge_weight, get_node, get_nodes, has_arrow, has_edge, has_node, accessibility, breadth_first_search, connected_components, depth_first_search, minimal_spanning_tree, mutual_accessibility, shortest_path, topological_sorting
 	"""
 
