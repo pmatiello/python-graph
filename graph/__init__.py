d1ca4cd8fe6e15525f7d14b8154357bb26f2590a
pmatiello <pmatiello@70df0079-b534-0410-988b-a5721c0f2d16>
2008-09-05 21:41:04 +0000

    docstrings

--- graph/__init__.py
+++ graph/__init__.py
@@ -54,7 +54,7 @@ class graph (object):
 	
 	Graphs are built of nodes and edges (or arrows).
 
-	@sort: __init__, __len__, __str__, generate, read, write, add_arrow, add_edge, add_graph, add_node, add_nodes, complete, add_spanning_tree, del_arrow, del_edge, get_arrow_label, get_arrow_weight, get_edge_label, get_edge_weight, get_edges, get_neighbors, get_nodes, has_arrow, has_edge, has_node, inverse, set_arrow_label, set_arrow_weight, set_edge_label, set_edge_weight, accessibility, breadth_first_search, connected_components, cut_edges, cut_nodes, depth_first_search, minimal_spanning_tree, mutual_accessibility, shortest_path, topological_sorting
+	@sort: __init__, __len__, __str__, generate, read, write, add_arrow, add_edge, add_graph, add_node, add_nodes, add_node_attribute, complete, add_spanning_tree, del_arrow, del_edge, get_arrow_label, get_arrow_weight, get_edge_label, get_edge_weight, get_edges, get_neighbors, get_nodes, get_node_attributes, has_arrow, has_edge, has_node, inverse, set_arrow_label, set_arrow_weight, set_edge_label, set_edge_weight, accessibility, breadth_first_search, connected_components, cut_edges, cut_nodes, depth_first_search, minimal_spanning_tree, mutual_accessibility, shortest_path, topological_sorting
 	"""
 
 
@@ -198,8 +198,8 @@ class graph (object):
 		@type  node: node
 		@param node: Node identifier.
 		
-		@type  attr: list
-		@param attr: List of node attributes specified as (attribute, value) tuples.
+		@type  attrs: list
+		@param attrs: List of node attributes specified as (attribute, value) tuples.
 		"""
 		if (not node in self.nodes.keys()):
 			self.nodes[node] = []
@@ -237,8 +237,8 @@ class graph (object):
 		@type  label: string
 		@param label: Edge label.
 		
-		@type  attr: list
-		@param attr: List of node attributes specified as (attribute, value) tuples.
+		@type  attrs: list
+		@param attrs: List of node attributes specified as (attribute, value) tuples.
 		"""
 		if (v not in self.nodes[u] and u not in self.nodes[v]):
 			self.nodes[u].append(v)
