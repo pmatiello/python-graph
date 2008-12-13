882834480a3df74b64a6558f3a681b7414247b7d
pmatiello <pmatiello@70df0079-b534-0410-988b-a5721c0f2d16>
2008-12-13 19:51:51 +0000

    correcting the credits in source files

--- graph/heuristics.py
+++ graph/heuristics.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2007-2008 Pedro Matiello <pmatiello@gmail.com>
-#                         Rhys Ulerich <rhys.ulerich@gmail.com>
+#                         Salim Fadhley <sal@stodge.org>
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
