a927afffd26c5e527b6c7c60738aed4f2bc90757
pmatiello <pmatiello@70df0079-b534-0410-988b-a5721c0f2d16>
2008-12-18 00:30:49 +0000

    added an A* heuristic for euclidean graphs

--- graph/tests/testAStar.py
+++ graph/tests/testAStar.py
@@ -5,25 +5,44 @@ import graph
 from test_data import nations_of_the_world
 
 class testAStar( unittest.TestCase ):
-    """
-    Test some very basic graph functions
-    """
-    
-    def setUp(self):
-        self.G = graph.graph()
-        nations_of_the_world(self.G)
-        
-    def testBasic1(self):
-        """
-        Test some very basic functionality
-        """
-        englands_neighbors = self.G.neighbors("England")
-        assert set(['Wales', 'Scotland', 'France', 'Ireland']) == set( englands_neighbors )
-        
-    def testAStar1(self):
-        heuristic = graph.heuristics.chow( "Wales", "North Korea", "Russia" )
-        heuristic.optimize( self.G )
-        result = graph.minmax.heuristic_search( self.G, "England", "India", heuristic )
-        
-        
-                
+	"""
+	Test some very basic graph functions
+	"""
+
+	def setUp(self):
+		self.G = graph.graph()
+		nations_of_the_world(self.G)
+
+	def testBasic1(self):
+		"""
+		Test some very basic functionality
+		"""
+		englands_neighbors = self.G.neighbors("England")
+		assert set(['Wales', 'Scotland', 'France', 'Ireland']) == set( englands_neighbors )
+
+	def testAStar1(self):
+		heuristic = graph.heuristics.chow( "Wales", "North Korea", "Russia" )
+		heuristic.optimize( self.G )
+		result = graph.minmax.heuristic_search( self.G, "England", "India", heuristic )
+
+class testAStarEuclideanHeuristic( unittest.TestCase ):
+	"""
+	Test the Euclidean heuristic for the A* algorithm.
+	"""
+
+	def setUp(self):
+		self.G = graph.graph()
+		self.G.add_node('A', [('position',[0,0])])
+		self.G.add_node('B', [('position',[2,0])])
+		self.G.add_node('C', [('position',[2,3])])
+		self.G.add_node('D', [('position',[1,2])])
+		self.G.add_edge('A', 'B')
+		self.G.add_edge('A', 'D')
+		self.G.add_edge('B', 'C')
+		self.G.add_edge('D', 'C')			
+
+	def testAStar1(self):
+		heuristic = graph.heuristics.euclidean()
+		heuristic.optimize(self.G)
+		result = graph.minmax.heuristic_search(self.G, 'A', 'C', heuristic )
+		print result
