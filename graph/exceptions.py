228467aaa53ca15570263b5101bd5358c182d47f
salimfadhley <salimfadhley@70df0079-b534-0410-988b-a5721c0f2d16>
2008-12-18 17:36:27 +0000

    change space-tabs to hard-tabs

--- graph/exceptions.py
+++ graph/exceptions.py
@@ -1,15 +1,15 @@
 
 class python_graph_exception( RuntimeError ):
-    """
-    A base-class for the various kinds of errors that occur in the the python-graph class.
-    """
-    
+	"""
+	A base-class for the various kinds of errors that occur in the the python-graph class.
+	"""
+	
 class unreachable( python_graph_exception ):
-    """
-    Goal could not be reached from start.
-    """
-    def __init__( self, start, goal ):
-        msg = "Node %s could not be reached from node %s" % ( repr(goal), repr(start) )
-        python_graph_exception.__init__( self, msg )
-        self.start = start
-        self.goal = goal
+	"""
+	Goal could not be reached from start.
+	"""
+	def __init__( self, start, goal ):
+		msg = "Node %s could not be reached from node %s" % ( repr(goal), repr(start) )
+		python_graph_exception.__init__( self, msg )
+		self.start = start
+		self.goal = goal
