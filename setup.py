afae57b0521b18572a699b0effae28770dfc520b
pmatiello <pmatiello@70df0079-b534-0410-988b-a5721c0f2d16>
2009-02-26 00:36:35 +0000

    minor: keywords in setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         author_email = "pmatiello@gmail.com",
         description = "A library for working with graphs in Python",
         license = "MIT",
-        keywords = "python graph algorithms library",
+        keywords = "python graphs hypergraphs networks library algorithms",
         url = "http://code.google.com/p/python-graph/",
         classifiers = ["License :: OSI Approved :: MIT License","Topic :: Software Development :: Libraries :: Python Modules"],
         long_description = "python-graph is a library for working with graphs in Python. This software provides a suitable data structure for representing graphs and a whole set of important algorithms.",
