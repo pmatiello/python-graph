4ad854ac36728eee514020be5f5db9c8d1e90c13
salimfadhley <salimfadhley@70df0079-b534-0410-988b-a5721c0f2d16>
2009-06-01 23:44:45 +0000

    More fixes to the new test-framework

--- tests/new_framework/test_main.py
+++ tests/new_framework/test_main.py
@@ -1,5 +1,6 @@
 import unittest
 import itertools
+import logging
 
 from pygraph.classes.Graph import graph
 from pygraph.classes.Digraph import digraph
@@ -8,10 +9,13 @@ from test_directed import test_directed
 from test_undirected import test_undirected
 from test_common import test_common
 
+log = logging.getLogger( __name__ )
+
 GRAPH_NODES = { "trivial":1,
+                "really_small":2,
                 "tiny":10,
                 "big":100,
-                #"huge":500 
+                "huge":250 
                 }
 
 GRAPH_EDGES = GRAPH_NODES
@@ -19,29 +23,48 @@ GRAPH_EDGES = GRAPH_NODES
 GRAPH_TYPES = { "graph":( graph, ( test_undirected, test_common )),
                 "digraph": (digraph, ( test_directed, test_common )), }
 
+if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger("").setLevel( logging.INFO )
+
+def valid_combo( nodes, edges ):
+    """
+    Exclude certain types of graph as they are too boring to test.
+    """
+    if nodes > 10:
+        return True
+    
+    if edges < nodes:
+        return True
+    
+    return False
+
 suite = unittest.TestSuite()
 
 for size_name, order in GRAPH_NODES.iteritems():
     for edge_size_name, edge_count in GRAPH_EDGES.iteritems():
-        for test_family, (test_class, base_classes) in GRAPH_TYPES.iteritems():
-            complete_bases = tuple( itertools.chain( base_classes ,[unittest.TestCase, ] ) )
-            
-            test_name = "test_o_%s_e_%s_%s" % ( size_name, edge_size_name, test_family )
-            
-            class_members = { "test_class":test_class,
-                              "graph_order":order,
-                              "edge_count":edge_count, }
-            
-            new_test_class = type( test_name,  complete_bases, class_members, )
-            globals()[test_name] = new_test_class
-            
-            
-            suite.addTests( unittest.TestLoader().loadTestsFromTestCase( new_test_class ) )
-            
-            del new_test_class
+        if valid_combo( order, edge_count ):    
+            for test_family, (test_class, base_classes) in GRAPH_TYPES.iteritems():
+                complete_bases = tuple( itertools.chain( base_classes ,[unittest.TestCase, ] ) )
+                
+                test_name = "test_o_%s_e_%s_%s" % ( size_name, edge_size_name, test_family )
+                log.info("Generating test-case %s" % test_name)
+                
+                class_members = { "test_class":test_class,
+                                  "graph_order":order,
+                                  "edge_count":edge_count, }
+                
+                new_test_class = type( test_name,  complete_bases, class_members, )
+                globals()[test_name] = new_test_class
+                
+                
+                suite.addTests( unittest.TestLoader().loadTestsFromTestCase( new_test_class ) )
+                
+                del new_test_class
         
 if __name__ == "__main__":
-    tr = unittest.TextTestRunner()
+    
+    tr = unittest.TextTestRunner( verbosity=2 )
     result = tr.run( suite )
     print repr(result)
         
--- tests/new_framework/test_undirected.py
+++ tests/new_framework/test_undirected.py
@@ -14,14 +14,15 @@ class test_undirected:
         For any graph G, the sum of the degrees of the verteces of G
         equals twice the number of edges of G.
         """
-        sum_of_the_degrees = sum( [ len( self.G[a] ) for a in self.G ] )
+        sum_of_the_degrees = sum( [ len( list( self.G[v] ) ) for v in self.G ] )
         number_of_edges = len( self.G.edges() )
-        assert sum_of_the_degrees == number_of_edges * 2
-        
+        assert sum_of_the_degrees == number_of_edges * 2, "sum of degrees: %i, num of edges: %i does not satisfy relationship" % ( sum_of_the_degrees, number_of_edges )
+         
     def test_vertex_odd_even(self):
         """
         Every graph contains an even number of odd verteces.
         """
-        odd_verteces = [ v for v in self.G if self.is_odd( len(self.G[v]) ) ]
+        odd_verteces = [  v for v in self.G if self.is_odd( len( list( self.G[v] ) ) ) ]
         assert not self.is_odd( len( odd_verteces ) )
+        
         
