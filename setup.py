029e67a502451db5c6a4c54081381eb3c0c1892a
pmatiello <pmatiello@70df0079-b534-0410-988b-a5721c0f2d16>
2009-06-09 01:40:20 +0000

    Version number up!

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError, ie:
 
 # Startup
 appname = "python-graph"
-appversion = "1.6.0"
+appversion = "1.6.1"
 
 # Extra files
 if (os.name == 'posix'):    # Files to be installed/packaged on Unix-like systems
