4aebe4e6fd78e070c2d53b6a502f7ff99ec16979
salimfadhley <salimfadhley@70df0079-b534-0410-988b-a5721c0f2d16>
2009-06-10 23:43:23 +0000

    Added the random seed feature to the new test framework.

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+from new_framework import *
--- tests/new_framework/test_common.py
+++ tests/new_framework/test_common.py
@@ -1,9 +1,12 @@
+import random
 from pygraph.algorithms.generators import generate
 
 class test_common:
     """
     Tests common to all graph types
     """
+    RANDOMSEED = 0
+    
     def printfoo( self ):
         return "foo"
     
@@ -12,13 +15,24 @@ class test_common:
         Generate a graph as per the classes spec
         """
         assert isinstance(self.test_class, type ), "Expected a graph class for testing, got %s" % repr( self.test_class )
+        self.G = self.makeTestGraph()
+        
+    def makeTestGraph(self):
         G = self.test_class()
+        random.seed( self.RANDOMSEED )
         generate(G, self.graph_order, self.edge_count)
-        self.G = G
+        return G
         
         
     def tearDown(self):
         del self.G
+        
+#    def testEquivalence(self):
+#        """
+#        Test that two identically constructed graphs are considered equal.
+#        """
+#        F = self.makeTestGraph()
+#        assert self.G == F
         
     def test_validate_node_edge_count(self):
         """
