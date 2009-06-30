534082685222e89cc8bb0d1b1da08a80aba4bc95
pmatiello <pmatiello@70df0079-b534-0410-988b-a5721c0f2d16>
2009-06-30 03:23:21 +0000

    Cleanup of readwrite tests + random graphs too.

--- tests/unittests-readwrite.py
+++ tests/unittests-readwrite.py
@@ -30,125 +30,50 @@ Unittests for graph.algorithms.readwrite
 import unittest
 import pygraph
 from pygraph.readwrite import *
-import time
+import testlib
+
+def graph_equality(gr1, gr2):
+    for each in gr1.nodes():
+        assert each in gr2.nodes()
+    for each in gr2.nodes():
+        assert each in gr1.nodes()
+    for each in gr1.edges():
+        assert each in gr2.edges()
+    for each in gr2.edges():
+        assert each in gr1.edges()
 
 class test_readwrite_dot(unittest.TestCase):
 
-    def _check_nodes(self, gr, dot):
-        dot = dot.split("\n")
-        for node in gr:
-            assert str(node) + ";" in dot
-
-    def _check_edges(self, gr, dot):
-        dot = dot.split("\n")
-        for node in gr:
-            for edge in gr[node]: 
-                assert ((str(node) + " -- " + str(edge) + ";" in dot) or
-                        (str(edge) + " -- " + str(node) + ";" in dot))
-
-    def _check_arrows(self, gr, dot):
-        dot = dot.split("\n")
-        for node in gr:
-            for edge in gr[node]: 
-                assert str(node) + " -> " + str(edge) + ";" in dot
-
-    def testWriteGraphDot(self):
-        gr = pygraph.graph()
-        gr.add_nodes([1, 2, 3, 4, 5])
-        gr.add_edge(1, 2)
-        gr.add_edge(2, 3)
-        gr.add_edge(2, 4)
-        gr.add_edge(4, 5)
-        gr.add_edge(1, 5)
-        gr.add_edge(3, 5)
+    def test_dot_for_graph(self):
+        gr = testlib.new_graph()
         dotstr = dot.write(gr)
-        self._check_nodes(gr, dotstr)
-        self._check_edges(gr, dotstr)
-
-    def testWriteDigraphDot(self):
-        gr = pygraph.digraph()
-        gr.add_nodes([1, 2, 3, 4, 5])
-        gr.add_edge(1, 2)
-        gr.add_edge(2, 3)
-        gr.add_edge(2, 4)
-        gr.add_edge(4, 5)
-        gr.add_edge(1, 5)
-        gr.add_edge(3, 5)
+        gr1 = dot.read(dotstr)
+        dotstr = dot.write(gr1)
+        gr2 = dot.read(dotstr)  
+        graph_equality(gr1, gr2)
+    
+    def test_dot_for_digraph(self):
+        gr = testlib.new_digraph()
         dotstr = dot.write(gr)
-        self._check_nodes(gr, dotstr)
-        self._check_arrows(gr, dotstr)
-
-    def testReadGraphDot(self):
-        dotstr = ['graph graphname {', '1;', '2;', '3;', '4;', '5;', '1 -- 2;', '3 -- 2;', '4 -- 5;', '1 -- 5;', '4 -- 2;', '5 -- 3;', '}', '']
-        dotstr = "\n".join(dotstr)
-        gr = dot.read(dotstr)
-        self._check_nodes(gr, dotstr)
-        self._check_edges(gr, dotstr)
-
-    def testReadDigraphDot(self):
-        dotstr = ['digraph graphname {', '1;', '2;', '3;', '4;', '5;', '1 -> 2;', '4 -> 5;', '1 -> 5;', '2 -> 3;', '2 -> 4;', '3 -> 5;', '}', '']
-        dotstr = "\n".join(dotstr)
-        gr = dot.read(dotstr)
-        self._check_nodes(gr, dotstr)
-        self._check_arrows(gr, dotstr)
+        gr1 = dot.read(dotstr)
+        dotstr = dot.write(gr1)
+        gr2 = dot.read(dotstr)  
+        graph_equality(gr1, gr2)
 
 class test_readwrite_markup(unittest.TestCase):
 
-    def _check_nodes(self, gr, xml):
-        xml = xml.split("\n")
-        print xml
-        for node in gr:
-            ok = False
-            for line in xml:
-                if("node" in line and str(node) in line):
-                    ok = True
-            assert ok
-
-    def _check_edges(self, gr, xml):
-        xml = xml.split("\n")
-        for node in gr:
-            for edge in gr[node]:
-                ok = False
-                for line in xml:
-                    if ("edge" in line and str(node) in line and str(edge) in line):
-                        ok = True
-                assert ok
-
-    def testWriteGraphXML(self):
-        gr = pygraph.graph()
-        gr.add_nodes([1, 2, 3, 4, 5])
-        gr.add_edge(1, 2)
-        gr.add_edge(2, 3)
-        gr.add_edge(2, 4)
-        gr.add_edge(4, 5)
-        gr.add_edge(1, 5)
-        gr.add_edge(3, 5)
-        xml = markup.write(gr)
-        self._check_nodes(gr, xml)
-        self._check_edges(gr, xml)
-
-    def testWriteDigraphXML(self):
-        gr = pygraph.digraph()
-        gr.add_nodes([1, 2, 3, 4, 5])
-        gr.add_edge(1, 2)
-        gr.add_edge(2, 3)
-        gr.add_edge(2, 4)
-        gr.add_edge(4, 5)
-        gr.add_edge(1, 5)
-        gr.add_edge(3, 5)
-        xml = markup.write(gr)
-        self._check_nodes(gr, xml)
-        self._check_edges(gr, xml)
-        print xml
-
-    def testReadGraphXML(self):
-        xml = '<?xml version="1.0" ?> <digraph>     <node id="1"/>     <node id="2"/>     <node id="3"/>     <node id="4"/>     <node id="5"/>     <edge from="1" label="" to="2" wt="1"/>     <edge from="4" label="" to="5" wt="1"/>     <edge from="1" label="" to="5" wt="1"/>     <edge from="2" label="" to="3" wt="1"/>     <edge from="2" label="" to="4" wt="1"/>     <edge from="3" label="" to="5" wt="1"/> </digraph>'
-        gr = markup.read(xml)
-        self._check_nodes(gr, xml)
-        self._check_edges(gr, xml)
-
-    def testReadDigraphXML(self):
-        xml = '<?xml version="1.0" ?> <digraph>     <node id="1"/>     <node id="2"/>     <node id="3"/>     <node id="4"/>     <node id="5"/>     <edge from="1" label="" to="2" wt="1"/>     <edge from="4" label="" to="5" wt="1"/>     <edge from="1" label="" to="5" wt="1"/>     <edge from="2" label="" to="3" wt="1"/>     <edge from="2" label="" to="4" wt="1"/>     <edge from="3" label="" to="5" wt="1"/> </digraph>'
-        gr = markup.read(xml)
-        self._check_nodes(gr, xml)
-        self._check_edges(gr, xml)
+    def test_xml_for_graph(self):
+        gr = testlib.new_graph()
+        dotstr = markup.write(gr)
+        gr1 = markup.read(dotstr)
+        dotstr = markup.write(gr1)
+        gr2 = markup.read(dotstr)  
+        graph_equality(gr1, gr2)
+    
+    def test_dot_for_digraph(self):
+        gr = testlib.new_digraph()
+        dotstr = markup.write(gr)
+        gr1 = markup.read(dotstr)
+        dotstr = markup.write(gr1)
+        gr2 = markup.read(dotstr)  
+        graph_equality(gr1, gr2)
