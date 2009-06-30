e6ce278151c8aa819d1a2c0f7b68d39398e4f0c1
pmatiello <pmatiello@70df0079-b534-0410-988b-a5721c0f2d16>
2009-06-30 05:22:08 +0000

    Copyright notices.

--- tests/testlib.py
+++ tests/testlib.py
@@ -1,7 +1,4 @@
 # Copyright (c) 2007-2009 Pedro Matiello <pmatiello@gmail.com>
-#                         Johannes Reinhardt <jreinhardt@ist-dein-freund.de>
-#                         Nathan Davis <davisn90210@gmail.com>
-#                         Zsolt Haraszti <zsolt@drawwell.net>
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
